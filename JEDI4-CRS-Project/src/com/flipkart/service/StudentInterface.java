1949f34d1f5b772357897b50700b846cf39b2b1a
shreyasjain999 <shreyasjain999@gmail.com>
2021-06-10 22:42:29 +0530

    Add Course Bean and StudentInterface

---
+++ JEDI4-CRS-Project/src/com/flipkart/service/StudentInterface.java
@@ -0,0 +1,24 @@
+package com.flipkart.service;
+
+public interface StudentInterface {
+	public void showCourses();
+    
+    public ArrayList<Grades> viewGrades(int studentId);
+
+    public void makePayment(Student student, String method);
+
+    public boolean addPrimaryCourse(Student student,int courseId);
+
+    public boolean removePrimaryCourse(Student student,int courseId);
+    
+    public boolean addSecondaryCourse(Student student,int courseId);
+
+    public boolean removeSecondaryCourse(Student student,int courseId);
+    
+    public boolean registerCourses(ArrayList<Integer> courseCart, Student student);
+
+    public void viewPrimaryRegisteredCourses(Student student);
+    
+    public void viewSecondaryRegisteredCourses(Student student);
+    
+}
